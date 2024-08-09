f05cdead9d1a832c827d6ade9b17ef029341ba74
Anastasia Nikitina <gunsnro@mail.ru>
2024-08-09 23:11:09 +0300

    first init

---
+++ cmd/grpc_client/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/fatih/color"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	desc "github.com/ananikitina/grpc/pkg/note_v1"
+)
+
+const (
+	address = "localhost:50051"
+	noteID  = 12
+)
+
+func main() {
+	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to connect to server:%v", err)
+	}
+	defer conn.Close()
+
+	c := desc.NewNoteV1Client(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := c.Get(ctx, &desc.GetRequest{Id: noteID})
+	if err != nil {
+		log.Fatalf("failed to get note by id: %d", noteID)
+	}
+
+	log.Printf(color.RedString("Note info:\n"), color.GreenString("%+v", r.GetNote()))
+
+}
